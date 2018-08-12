0bf9035f0b01cc6594ca47ede011407217bd300a
Victor <em3888@gmail.com>
2018-08-12 18:14:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Working with IPFS in React
+
+## CORS
+
+`ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST", "OPTIONS"]'
+`
+
+`ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'`
+
+`ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'`
+
+`ipfs config --json API.HTTPHeaders.Access-Control-Allow-Headers '["Authorization"]`
+
+`ipfs config --json API.HTTPHeaders.Access-Control-Expose-Headers '["Location"]'`
+
+
---
+++ src/storehash.js
@@ -0,0 +1,37 @@
+import web3 from './web3';
+
+const address = "0x9d2cca961249716f0a1dd805f7bc9e09a05e9c5c";
+
+
+const abi = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "x",
+                "type": "string"
+            }
+        ],
+        "name": "setHash",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "getHash",
+        "outputs": [
+            {
+                "name": "x",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+
+export default new web3.eth.Contract(abi, address)
